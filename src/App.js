4904edfa86fa8d0cdbafc0515cf39ff762a103b6
Soo Kim <chizbob@gmail.com>
2018-12-07 10:15:07 -0800

    room list rendered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
+
+  var config = {
+    apiKey: "AIzaSyAZ-nh81214MgEN6E0-MO-rr18njhp0f8k",
+    authDomain: "react-chatroom-8e6ec.firebaseapp.com",
+    databaseURL: "https://react-chatroom-8e6ec.firebaseio.com",
+    projectId: "react-chatroom-8e6ec",
+    storageBucket: "react-chatroom-8e6ec.appspot.com",
+    messagingSenderId: "458261199253"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList
+        firebase = {firebase}
+         />
       </div>
     );
   }
